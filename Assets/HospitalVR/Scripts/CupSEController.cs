90da3cd44fbf78f1bfd4b7a2f8417c85d8482d05
riekure <s2darl0uvgia24@gmail.com>
2019-09-10 18:49:30 +0900

    add cup sound

---
+++ Assets/HospitalVR/Scripts/CupSEController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupSEController : MonoBehaviour 
+{
+
+    private AudioSource cupDropSE;  // 転がる音
+
+    // Use this for initialization
+    void Start () 
+    {
+        cupDropSE = GetComponent<AudioSource>();
+    }
+
+    // ものとぶつかったときに音を鳴らす
+    private void OnCollisionEnter(Collision other)
+    {
+        // 音を鳴らす
+        cupDropSE.PlayOneShot(cupDropSE.clip);
+    }
+}
