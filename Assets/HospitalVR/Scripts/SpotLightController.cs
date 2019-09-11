6a702418aae13e7333651c2551b0b425d338dbc5
riekure <s2darl0uvgia24@gmail.com>
2019-09-11 11:34:23 +0900

    add spot light

---
+++ Assets/HospitalVR/Scripts/SpotLightController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightController : MonoBehaviour {
+
+    private Light spotLight;  // ON/OFFするライト
+
+    // Use this for initialization
+    void Start ()
+    {
+        spotLight = GetComponent<Light>();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    public void OnClickLightButton()
+    {
+        spotLight.enabled = !spotLight.enabled;
+    }
+}
